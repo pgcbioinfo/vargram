8137107acb83f78a0909b391930936e7a3c720c5
CJ <cbpalpallatoc@up.edu.ph>
2024-05-10 17:37:51 +0800

    Completed initial attribute values

--- src/vargram/plots/bar.py
+++ src/vargram/plots/bar.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mc
+import numpy as np
+
+def create_default_colors(num_color):
+    cmap_name = 'viridis'  
+    cmap = plt.get_cmap(cmap_name)
+    listed_cmap = cmap(np.linspace(0, 1, num_color))
+    return [mc.to_hex(color) for color in listed_cmap]
 
 class Bar():
 
@@ -6,6 +14,7 @@ class Bar():
 
         # Data
         self.data = data
+        num_batch_initial = len(self.data['batch'].unique())
 
         # Plot attributes
         self.fig = plt.figure()
@@ -13,26 +22,26 @@ class Bar():
         # Data attributes
         self.x = 'mutation'
         self.xorder = 'position'
-        self.y = 'Instances'
+        self.y = '' # Indicating the yvalues are counts or weights of unique mutations
         self.group = 'gene'
         self.stack = 'batch'
 
         # Aesthetic attributes
-        self.xticks.font
-        self.xticks.fontsize
+        self.xticks.fontsize = 6
         self.xticks.rotation = 90
 
-        self.yticks.font
-        self.yticks.fontsize
+        self.yticks.fontsize = 7
+        self.ylabel = ['Instances' if num_batch_initial > 0 else 'Weights']
+        self.ylabel.fontsize = 'medium'
 
-        self.group.font
-        self.group.fontsize
-        self.group.label
+        self.key.fontsize = 8
 
-        self.stack.font 
-        self.stack.fontsize
-        self.stack.color
-        self.stack.label 
+        self.group.fontsize = 'small'
+        self.group.label = 'Gene'
+
+        self.stack.fontsize = 'medium'
+        self.stack.color =  create_default_colors(num_batch_initial)
+        self.stack.label = 'Batch'
 
     def process(self, **process_kwargs):
         print('Processed data for barplot.')
